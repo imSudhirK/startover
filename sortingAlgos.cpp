2983ff0a9cafe511cd134e6bad1fcdf1cec3a789
Sudhir Kumar <sudhir.kumar@credflow.in>
2023-08-01 23:21:05 +0530

    quick-sort and merge-sort revised

--- sortingAlgos.cpp
+++ sortingAlgos.cpp
@@ -7,6 +7,12 @@ void bubbleSort(int arr[], int n);
 void selectionSort(int arr[], int n);
 void insertionSort(int arr[], int n);
 
+int partitionArr(int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
+
+void mergeArr(int arr[], int left, int mid, int right);
+void mergeSort(int arr[], int begin, int end);
+
 
 int main() {
 	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
@@ -14,7 +20,9 @@ int main() {
 	printArray(arr, n);
 	// bubbleSort(arr, n);
 	// selectionSort(arr, n);
-	insertionSort(arr, n);
+	// insertionSort(arr, n);
+	// quickSort(arr, 0, n-1);
+	mergeSort(arr, 0, n-1);
 	printArray(arr, n);
 
 
@@ -49,4 +57,66 @@ void insertionSort(int arr[], int n){
 			swapVals(&arr[j], &arr[j+1]);
 		}
 	}
+}
+
+// takes pivot (last element) and places smaller to left and greater to right of pivot
+int partitionArr(int arr[], int low, int high){
+	int pivot = arr[high];
+	int partitioningIndex = low;
+	for(int j = low; j <= high - 1; j++){
+		if(arr[j] >= pivot) continue;
+		if(j != partitioningIndex) swapVals(&arr[j], &arr[partitioningIndex]);
+		partitioningIndex++;
+	}
+	swapVals(&arr[high], &arr[partitioningIndex]);
+	return partitioningIndex;
+}
+
+// quick sort,  time: O(n log n), space: O(1)
+void quickSort(int arr[], int low, int high){
+	if(low >= high) return;
+	int partitioningIndex = partitionArr(arr, low, high);
+	quickSort(arr, low, partitioningIndex - 1);
+	quickSort(arr, partitioningIndex + 1, high);
+}
+
+// merges two subarrays of array
+void mergeArr(int arr[], int begin, int mid, int end){
+	int lenLeftArr = mid - begin + 1;
+	int lenRightArr = end - mid;
+	int *leftArr = new int[lenLeftArr], *rightArr = new int[lenRightArr];
+	for(int i = 0; i < lenLeftArr; i++) leftArr[i] = arr[begin + i];
+	for(int i = 0; i < lenRightArr; i++) rightArr[i] = arr[mid + 1 + i];
+	int indLeftArr = 0, indRightArr = 0, indMergedArr = begin;
+	while(indLeftArr < lenLeftArr && indRightArr < lenRightArr){
+		if(leftArr[indLeftArr] > rightArr[indRightArr]){
+			arr[indMergedArr] = rightArr[indRightArr];
+			indRightArr++;
+		} else {
+			arr[indMergedArr] = leftArr[indLeftArr];
+			indLeftArr++;
+		}
+		indMergedArr++;
+	}
+	while(indLeftArr < lenLeftArr){
+		arr[indMergedArr] = leftArr[indLeftArr];
+		indLeftArr++;
+		indMergedArr++;
+	}
+	while(indRightArr < lenRightArr){
+		arr[indMergedArr] = rightArr[indRightArr];
+		indRightArr++;
+		indMergedArr++;
+	}
+	delete[] leftArr;
+	delete[] rightArr;
+}
+
+// merge sort, time: O(n log n); space: O(n)
+void mergeSort(int arr[], int begin, int end){
+	if(begin >= end) return;
+	int mid = (begin + end)/2;
+	mergeSort(arr, begin, mid);
+	mergeSort(arr, mid + 1, end);
+	mergeArr(arr, begin, mid, end);
 }
